ac50571cff2a668a08fea160e109feea229b84b5
Tarun-Das-au8 <imtarun79@gmail.com>
2021-06-05 09:37:59 +0530

    slug component:

---
+++ pages/listing/[id].js
@@ -0,0 +1,38 @@
+export const getStaticPaths = async () =>{
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json()
+
+    const paths = data.map(item =>{
+        return{
+            params: {id : item.id.toString()}
+        }
+    })
+
+    return{
+        paths,
+        fallback: false
+    }
+}
+
+export const getStaticProps = async (context) =>{
+    const id = context.params.id
+    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+    const data = await res.json()
+    console.log(data)
+    return {
+        props: {item:data}
+    }
+}
+
+const Details = ({item}) => {
+    return (
+        <div>
+            <h2>{item.name}</h2>
+            <p>{item.email}</p>
+            <p>{item.website}</p>
+            <p>{item.address.city}</p>
+        </div>
+    );
+}
+ 
+export default Details;
